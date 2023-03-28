18a7c632d48469ad37cf084140e16754070fd875
anbaitiakova <nasa201116@mail.ru>
2023-03-29 00:17:18 +0300

    lab 1

---
+++ homework-01/main.py
@@ -0,0 +1,9 @@
+import LRUCache
+
+cache = LRUCache.LRUCache(100)
+cache.set('Jesse', 'Pinkman')
+cache.set('Walter', 'White')
+cache.set('Jesse', 'James')
+cache.get('Jesse') # вернёт 'James'
+cache.rem('Walter')
+cache.get('Walter') # вернёт ''
