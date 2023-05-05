ba537ad9fa642d2d8d4c4c12ba94741be5266ff8
anbaitiakova <nasa201116@mail.ru>
2023-05-06 01:44:11 +0300

    lab 4 need to migrate

--- homework-django/mysite/bookShop/models.py
+++ homework-django/mysite/bookShop/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}, {self.country}"
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    published_date = models.DateField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+
+class Sale(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    sale_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.customer} bought {self.book} on {self.sale_date}"
